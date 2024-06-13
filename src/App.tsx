513a5acf585b6352f8d2058b31db3db4720e5e38
Gordus Alexander <aldrmiller@gmail.com>
2024-06-13 23:39:15 +0600

    реализовал логику компонента AddMovieForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import AddMovieForm from './components/AddMovieForm/AddMovieForm';
 
 const App = () => {
   return (
-    <>
-
-    </>
+    <div className="container-wrapper">
+      <AddMovieForm />
+    </div>
   );
 };
 
---
+++ src/components/AddMovieForm/AddMovieForm.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {ChangeEvent, useState} from 'react';
+import './AddMovieForm.css';
+
+interface Props {
+  onSubmit: (message: string) => void;
+}
+
+const AddMovieForm: React.FC<Props> = ({onSubmit}) => {
+  const [movieName, setMovieName] = useState<string>('');
+
+  const onFieldChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const valueInput = (event.target as HTMLTextAreaElement).value;
+    setMovieName(valueInput);
+  };
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (movieName.trim().length > 0) {
+      onSubmit(movieName);
+      setMovieName('');
+    }
+  };
+
+  return (
+    <form className="form-movie" onSubmit={onFormSubmit}>
+      <textarea className="movie-item" value={movieName} required placeholder="Enter movie name" onChange={onFieldChange}></textarea>
+      <button className="button-submit" type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddMovieForm;
